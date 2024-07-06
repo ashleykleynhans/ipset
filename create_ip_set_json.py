bcb46fd8a076a7ca2bcae7d9d43bcdc6913b6945
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-07-06 15:19:49 +0200

    Sort the IP addresses

--- create_ip_set_json.py
+++ create_ip_set_json.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import csv
-import json
+import ipaddress
 
 
 def create_ip_set_json():
     # Read CSV data from file
-    with open('ipset.csv', mode='r') as file:
+    with open('ipv4.csv', mode='r') as file:
         reader = csv.DictReader(file)
 
         # Extract IP addresses
-        ip_addresses = [row['IP'] + '/32' for row in reader]
+        ip_list = [row['IP'] + '/32' for row in reader]
 
-    # Output as JSON array
-    print(json.dumps(ip_addresses, indent=4))
+    # Convert to a list of IP networks and sort
+    sorted_ip_list = sorted(ip_list, key=lambda ip: ipaddress.ip_network(ip, strict=False))
+
+    # Output the sorted list in list format with each IP on a new line
+    output_list = "[\n" + ",\n".join(f'    "{ip}"' for ip in sorted_ip_list) + "\n]"
+    print(output_list)
 
 
 if __name__ == '__main__':
